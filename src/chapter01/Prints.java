aecaac562b6bf5e3c912dd3b43fcceb783dfd19c
hernan <hernan97carp@hotmail.com>
2024-10-15 21:21:01 -0300

    first commit

---
+++ src/chapter01/Prints.java
@@ -0,0 +1,15 @@
+package chapter01;
+
+
+public class Prints {
+	
+	public static void main(String[] args) {
+		System.out.println("Hello");
+		System.out.print("Hello");
+		System.out.printf("Age: %d", 25);
+		System.out.format("Score: %.2f",98.5);
+		System.err.println("Error message");
+		
+	}
+
+}
